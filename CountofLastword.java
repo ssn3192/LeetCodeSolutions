81d75eb417094dd43ba8a5f5e985c1a5b3eaf9a2
Shivani Narang <shivu.narang@gmail.com>
2016-04-04 18:56:50 -0500

    Update CountofLastword.java

--- CountofLastword.java
+++ CountofLastword.java
@@ -9,21 +9,38 @@ import java.util.*;
 import java.util.Scanner.*;
 public class CountofLastword {
 
-	public void Lengthoflastword(String word){
+	public int Lengthoflastword(String word){
 		
 		if(word.length()==0)
-			return;
+			return 0;
 		
      String lastword= word.substring(word.lastIndexOf(" ")+1);
 		
-		System.out.print("last word of the sentence :" +lastword);
+		System.out.print("last word :" +lastword);
 				
 		int len= lastword.length();
 		System.out.println();
-		System.out.print("Length of last word:" +len);
+		System.out.println(len);
+		return len;
+		}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    CountofLastword ob= new CountofLastword();
+
+		
+		String w;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter string");
+		
+		w= sc.nextLine();
+		
+		ob.Lengthoflastword(w);
+		
 		
 		
 	}
+
+}
 	
 	
 	public static void main(String[] args) {

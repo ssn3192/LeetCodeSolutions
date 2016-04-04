e93195f1f05f77f748f4d4b397669f185d782501
Shivani Narang <shivu.narang@gmail.com>
2016-04-04 17:34:12 -0500

    Added files via upload

---
+++ CountofLastword.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.util.Scanner.*;
+public class CountofLastword {
+
+	public void Lengthoflastword(String word){
+		
+		
+     String lastword= word.substring(word.lastIndexOf(" ")+1);
+		
+		System.out.print("last word :" +lastword);
+				
+		int len= lastword.length();
+		System.out.println();
+		System.out.println(len);
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    CountofLastword ob= new CountofLastword();
+
+		
+		String w;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter string");
+		
+		w= sc.nextLine();
+		
+		ob.Lengthoflastword(w);
+		
+		
+		
+	}
+
+}

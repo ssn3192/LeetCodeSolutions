6c54582c07ab82bc4bb5550986bd30b21594a9b6
Shivani Narang <shivu.narang@gmail.com>
2016-04-04 19:22:34 -0500

    Added files via upload

---
+++ ReverseInt.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.Scanner.*;
+public class ReverseInt {
+	
+	
+    public int reverse(int x) {
+        
+int reverse=0;
+		
+		while(x!=0)
+		{			
+			reverse= reverse*10;
+		    reverse= reverse + x%10;
+		    x= x/10;
+		    
+		}
+	System.out.println("Reverse Number:" +reverse);
+		return reverse;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the number");
+		int number= sc.nextInt();
+		
+		ReverseInt rev= new ReverseInt();
+		rev.reverse(number);
+		
+	}
+
+}
